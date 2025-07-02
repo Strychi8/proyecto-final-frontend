c62c386d3752a4d31b25572ea3c916a949f419c9
Strychi8 <tomas2000T@gmail.com>
2025-07-02 13:40:53 -0300

    Implementacion de los archivos HTML, CSS y JavaScript del Carrito de Compras

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,39 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
+
+const listaProductos = document.querySelector("#listaProductos");
+
+let productsArray = [];
+
+document.addEventListener('DOMContentLoaded', function (){
+    eventListeners();
+})
+
+function eventListeners(){
+    listaProductos.addEventListener("click", getDataElements);
+}
+
+function getDataElements(evento){
+    if(evento.target.classList.contains("agregar-producto")){
+       const elementHtml = evento.target.parentElement.parentElement;
+       // console.log(elementHtml);
+       selectData(elementHtml);
+    }
+}
+
+function selectData(producto){
+    const productoObjeto = {
+        img: producto.querySelector("img").src,
+        title: producto.querySelector("h3").textContent,
+        price: parseFloat(producto.querySelector("span").textContent.replace("$","")),
+        id: parseInt(producto.querySelector('button[type="button"]').dataset.id, 10),
+        quantity: 1
+    }
+
+    // console.log(productoObjeto)
+    productsArray = [...productsArray, productoObjeto];
+    //showAlert
+    console.log(productsArray);
+    // productsHtml();
+}
